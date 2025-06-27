9464e72ebb194e166f0c3be76f73d5fdc9f37aae
Dissimuloniel <112813808+Dissimuloniel@users.noreply.github.com>
2025-06-27 23:04:12 +0300

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 
-
 class CurrentUserOrAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
@@ -9,7 +8,6 @@ class CurrentUserOrAdminOrReadOnly(permissions.BasePermission):
         if obj == user:
             return True
         return user.is_staff
-
 
 class IsAuthor(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
